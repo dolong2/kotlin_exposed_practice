8d6bb3a6b41b62936b0efb8c7118a53c21899bc0
jeayoung <k01066624566@gmail.com>
2023-05-01 14:43:34 +0900

    aop 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 }
 
 tasks.withType<KotlinCompile> {
