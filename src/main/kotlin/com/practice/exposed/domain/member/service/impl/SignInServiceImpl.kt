6420e464c976f67506c56d4299f97f9be20eb915
jeayoung <k01066624566@gmail.com>
2023-05-10 08:56:57 +0900

    PasswordMismatchException 적용

--- src/main/kotlin/com/practice/exposed/domain/member/service/impl/SignInServiceImpl.kt
+++ src/main/kotlin/com/practice/exposed/domain/member/service/impl/SignInServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.practice.exposed.domain.member.service.impl
 
 import com.practice.exposed.domain.member.exception.MemberNotFoundException
+import com.practice.exposed.domain.member.exception.PasswordMismatchException
 import com.practice.exposed.domain.member.persenation.request.SignInRequest
 import com.practice.exposed.domain.member.persenation.response.SignInResponse
 import com.practice.exposed.domain.member.repository.MemberRepository
@@ -21,7 +22,7 @@ class SignInServiceImpl(
         val member = (memberRepository.findByEmail(signInRequest.email)
             ?: throw MemberNotFoundException())
         if (!passwordEncoder.matches(signInRequest.password, member.password))
-            throw RuntimeException()
+            throw PasswordMismatchException()
         val roles = roleRepository.findByMember(member)
         val accessToken = tokenProvider.generateAccessToken(member.email, roles.map { it.role })
         val refreshToken = tokenProvider.generateRefreshToken(member.email, roles.map { it.role })
