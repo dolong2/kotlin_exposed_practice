1ab21dc7087cb464f9a384ce7d9fddb9a001cd64
jeayoung <k01066624566@gmail.com>
2023-05-18 16:03:29 +0900

    remove Transactional annotation

--- src/main/kotlin/com/practice/exposed/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/practice/exposed/global/security/auth/AuthDetailsService.kt
@@ -5,10 +5,8 @@ import com.practice.exposed.domain.member.repository.MemberRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
 class AuthDetailsService(
     private val memberRepository: MemberRepository
 ) : UserDetailsService {
